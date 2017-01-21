2d2c697c94812d23608e490a5c96ea70c8ad48d0
Damian Hites <nizebulous@gmail.com>
2017-01-21 10:34:40 -0500

    Fix references to project2 -> pib

--- PiB/urls.py
+++ PiB/urls.py
@@ -1,4 +1,4 @@
-"""project2 URL Configuration
+"""pib URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
