143279b548ef3fb75b529dca758473efd405347f
Damian Hites <nizebulous@gmail.com>
2017-02-10 22:49:54 -0500

    Add the dev setup for the allowed hosts.

--- pib/production_settings.py
+++ pib/production_settings.py
@@ -1,7 +1,7 @@
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['physicsisbeautiful.com']
+ALLOWED_HOSTS = ['physicsisbeautiful.com', 'physicsisbeautiful.com:8000']
 
 
 from pib.common_settings import *  # noqa: E402, F401
