d218f841f72d32859dccc1870926d18441d35bfb
Nicolas Scozzaro <nscozzaro@gmail.com>
2017-02-20 19:05:07 -0500

    Added hint collapsible element

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -554,6 +554,18 @@ class Question extends React.Component {
         if (this.props.question.image) {
             image = <img src={'/' + this.props.question.image}/>;
         }
+        var hint = '';
+        if (this.props.question.hint) {
+            hint =
+                <div className = "hintDiv">
+                    <div className="hintButton">
+                        <a href="#demo" data-toggle="collapse">hint</a>
+                    </div>
+                    <div id="demo" className="collapse">
+                        {this.props.question.hint}
+                    </div>
+                </div>;
+        }
         var answerField = '';
         if (this.props.question.question_type == "SINGLE_ANSWER") {
             answerField = <VectorCanvas question={this.props.question} answer={this.props.answer}/>;
@@ -565,7 +577,8 @@ class Question extends React.Component {
                 <div className="row">
                     <div className="col-md-6 text-center">
                         <div className="bounding-box">
-                            <h1 id="ajaxDiv">{this.props.question.text + ' ' + this.props.question.hint}</h1>
+                            <h1 id="ajaxDiv">{this.props.question.text}</h1>
+                            {hint}
                             <div className="thumbnail">
                                 {image}
                             </div>
