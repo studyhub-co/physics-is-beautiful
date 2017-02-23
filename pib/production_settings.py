24b6c5ea1a43db99426e4e3a7fe28a5c770ed428
Damian Hites <nizebulous@gmail.com>
2017-02-22 23:27:56 -0500

    Use Amazon SES as email sending backend.

--- pib/production_settings.py
+++ pib/production_settings.py
@@ -5,3 +5,10 @@ ALLOWED_HOSTS = ['physicsisbeautiful.com', 'physicsisbeautiful.com:8000']
 
 
 from pib.common_settings import *  # noqa: E402, F401
+
+
+EMAIL_BACKEND = 'django_ses.SESBackend'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_SES_REGION_NAME = 'us-east-1'
+AWS_SES_REGION_ENDPOINT = 'email.us-east-1.amazonaws.com'
