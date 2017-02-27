4dcea1a620b7939ac21032026f9624ac0c23749b
Damian Hites <nizebulous@gmail.com>
2017-02-26 22:03:21 -0500

    Update for raven config.

--- pib/production_settings.py
+++ pib/production_settings.py
@@ -1,3 +1,7 @@
+import os
+import raven
+
+
 DEBUG = False
 
 
@@ -17,5 +21,5 @@ RAVEN_CONFIG = {
     'dsn': os.getenv('RAVEN_DSN'),
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    # 'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
 }
