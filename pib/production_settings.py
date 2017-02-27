f5eea236eeaf3bd5bcf0d662010e4c7658ec131e
Damian Hites <nizebulous@gmail.com>
2017-02-26 21:04:47 -0500

    Try not setting the release version.

--- pib/production_settings.py
+++ pib/production_settings.py
@@ -17,5 +17,5 @@ RAVEN_CONFIG = {
     'dsn': os.getenv('RAVEN_DSN'),
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    # 'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
 }
