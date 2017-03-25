457e75dcddefec13e1d1073753ed290816ebd27c
Nicolas Scozzaro <nscozzaro@gmail.com>
2017-03-24 23:42:17 -0400

    Adding more blog-app

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-
 from . import views
 
 
@@ -7,4 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     url(r'^$', views.BlogHomepage, name='bloghomepage'),
+    url(r'^college_scorecard/$', views.CollegeScorecard, name='college_scorecard'),
 ]
