ff7b24f429d488cd666f1e451a93d12e1766a502
Nicolas Scozzaro <nscozzaro@gmail.com>
2017-04-01 17:07:59 -0400

    Added blog app

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def BlogHomepage(request):
+    return render(request, 'blog/blog_homepage.html')
+
+def CollegeScorecard(request):
+    return render(request, 'blog/college_scorecard.html')
