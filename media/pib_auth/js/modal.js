6fc8f4e0c2613d0b9c2433dc2d019dbe272c2eef
Nicolas Scozzaro <nscozzaro@gmail.com>
2017-06-08 20:51:12 -0400

    Deleted the media folder

--- media/pib_auth/js/modal.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    $(".auth-modal-swap").click(function() {
-        $("#login-modal").modal('toggle');
-        $("#signup-modal").modal('toggle');
-    });
-    $("#logout-action").click(function() {
-        $("#logout-form").submit();
-    });
-}
