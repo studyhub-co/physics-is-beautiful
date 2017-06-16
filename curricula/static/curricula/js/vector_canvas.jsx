0f63ba179ff029fdd7200c3661ea07ce0ce7ff27
Damian Hites <nizebulous@gmail.com>
2017-06-15 23:02:48 -0400

    Fix for null vector problems.

--- curricula/static/curricula/js/vector_canvas.jsx
+++ curricula/static/curricula/js/vector_canvas.jsx
@@ -335,6 +335,7 @@ export class VectorCanvas extends React.Component {
         var newState = {};
         if (this.props.clear && this.state.checked) {
             newState.checked = false;
+            console.log('CLEARING CHECK');
         }
         if (this.state.submitted && this.props.clear) {
             newState.submitted = false;
@@ -393,6 +394,7 @@ export class VectorCanvas extends React.Component {
 
     nullBoxCheck(event) {
         var newState = !this.state.checked;
+        console.log('NEW STATE: ' + newState);
         if (newState) {
             if (this.arrow) {
                 this.arrow.delete();
