d5e3cc381e0f931d7779c8ba204745c1d7b680e4
LennyLip <vermus.jabber@gmail.com>
2017-11-12 11:51:10 +0300

    disable f command in mathquill (mathquill monkey patching)

--- curricula/static/curricula/mathquill-0.10.1/mathquill.js
+++ curricula/static/curricula/mathquill-0.10.1/mathquill.js
@@ -3854,15 +3854,15 @@ LatexCmds.operatorname = P(MathCommand, function(_) {
   };
 });
 
-LatexCmds.f = P(Letter, function(_, super_) {
-  _.init = function() {
-    Symbol.p.init.call(this, this.letter = 'f', '<var class="mq-f">f</var>');
-  };
-  _.italicize = function(bool) {
-    this.jQ.html('f').toggleClass('mq-f', bool);
-    return super_.italicize.apply(this, arguments);
-  };
-});
+// LatexCmds.f = P(Letter, function(_, super_) {
+//   _.init = function() {
+//     Symbol.p.init.call(this, this.letter = 'f', '<var class="mq-f">f</var>');
+//   };
+//   _.italicize = function(bool) {
+//     this.jQ.html('f').toggleClass('mq-f', bool);
+//     return super_.italicize.apply(this, arguments);
+//   };
+// });
 
 // VanillaSymbol's
 LatexCmds[' '] = LatexCmds.space = bind(VanillaSymbol, '\\ ', '&nbsp;');
