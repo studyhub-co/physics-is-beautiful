199aee0a15561846df78266c682e0d1797235166
LennyLip <vermus.jabber@gmail.com>
2017-12-24 21:44:10 +0300

    UCPT: removed UCPT field, ANSWER NUMBER not work for now

--- curricula/widgets.py
+++ curricula/widgets.py
@@ -1,14 +1,24 @@
 from django.forms.widgets import MultiWidget
 from django.forms.widgets import TextInput
 
+# class UCWidget(MultiWidget):
+#     template_name = 'django/forms/widgets/multiwidget.html'
+#
+#     def decompress(self, value):
+#         if value:
+#             return value
+#         return [None, None, None, None]
+#
+#     class Media:
+#         js = ('/static/curricula/admin/js/math.min.js',
+#               '/static/curricula/admin/js/get-units.js',
+#               '/static/curricula/admin/js/quantities.js',
+#               '/static/curricula/admin/js/uc_utils.js',
+#               )
 
-class UCWidget(MultiWidget):
-    template_name = 'django/forms/widgets/multiwidget.html'
 
-    def decompress(self, value):
-        if value:
-            return value
-        return [None, None, None, None]
+class MathQuillWidget(TextInput):
+    template_name = 'curricula/widgets/mq_widget.html'
 
     class Media:
         js = ('/static/curricula/admin/js/math.min.js',
@@ -17,10 +27,13 @@ class UCWidget(MultiWidget):
               '/static/curricula/admin/js/uc_utils.js',
               )
 
-
-class MathQuillWidget(TextInput):
-    template_name = 'curricula/widgets/mq_widget.html'
-
-
 class UnitNameWidget(TextInput):
     template_name = 'curricula/widgets/uc_widget.html'
+
+    class Media:
+        js = ('/static/curricula/admin/js/math.min.js',
+              '/static/curricula/admin/js/get-units.js',
+              '/static/curricula/admin/js/quantities.js',
+              '/static/curricula/admin/js/uc_utils.js',
+              )
+
