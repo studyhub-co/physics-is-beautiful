db3493c26c3117b1f1dd5055e7d8dbc185fd43f0
whoami <admin@simlit.com>
2018-01-11 17:57:03 +0300

    math entry problem type: implemented mathquill box in the admin

--- curricula/widgets.py
+++ curricula/widgets.py
@@ -1,32 +1,8 @@
-# from django.template.loader import render_to_string
-# from django.utils.safestring import mark_safe
-#
-# from django.contrib.contenttypes.models import ContentType
-#
-# import json
-#
-# from django.forms.widgets import MultiWidget
 from django.forms.widgets import TextInput
-# from django.forms.widgets import Widget
-
-# class UCWidget(MultiWidget):
-#     template_name = 'django/forms/widgets/multiwidget.html'
-#
-#     def decompress(self, value):
-#         if value:
-#             return value
-#         return [None, None, None, None]
-#
-#     class Media:
-#         js = ('/static/curricula/admin/js/math.min.js',
-#               '/static/curricula/admin/js/get-units.js',
-#               '/static/curricula/admin/js/quantities.js',
-#               '/static/curricula/admin/js/uc_utils.js',
-#               )
 
 
-class MathQuillWidget(TextInput):
-    template_name = 'curricula/widgets/mq_widget.html'
+class MathQuillUnitConversionWidget(TextInput):
+    template_name = 'curricula/widgets/conversion/mq_widget.html'
 
     class Media:
         js = ('/static/curricula/admin/js/math.min.js',
@@ -37,7 +13,7 @@ class MathQuillWidget(TextInput):
 
 
 class UnitNameWidget(TextInput):
-    template_name = 'curricula/widgets/uc_widget.html'
+    template_name = 'curricula/widgets/conversion/uc_widget.html'
 
     class Media:
         js = ('/static/curricula/admin/js/math.min.js',
@@ -48,23 +24,7 @@ class UnitNameWidget(TextInput):
 
 
 class ConversionStepsJSONWidget(TextInput):
-    template_name = 'curricula/widgets/conversion_steps_json_widget.html'
-
-    # def render(self, name, value, attrs=None):
-    #     # if not value:
-    #     #     value = '[{"numerator":"", "denominator":""},' \
-    #     #             '{"numerator":"", "denominator":""},' \
-    #     #             '{"numerator":"", "denominator":""},' \
-    #     #             '{"numerator":"", "denominator":""}]'
-    #     # else:
-    #     value = json.dumps(value)
-    #
-    #     context = {
-    #         'data': value,
-    #         'name': name
-    #     }
-    #
-    #     return mark_safe(render_to_string(self.template_name, context))
+    template_name = 'curricula/widgets/conversion/steps_json_widget.html'
 
     class Media:
         js = ('/static/curricula/admin/js/math.min.js',
@@ -73,3 +33,7 @@ class ConversionStepsJSONWidget(TextInput):
               '/static/curricula/admin/js/uc_widgets.js',
               '/static/curricula/admin/js/conversion_steps_widget.js',
               )
+
+
+class MathConversionWidget(TextInput):
+    template_name = 'curricula/widgets/mq_widget.html'
