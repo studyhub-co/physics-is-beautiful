387bb41e51e6f92456cce90b9e36bf6bd6f0c174
LennyLip <vermus.jabber@gmail.com>
2018-02-02 11:44:36 +0300

    MutiSelect Question type: added card and pretty checkbox

--- curricula/static/curricula/js/components/answers/choices/image_with_text_choice.jsx
+++ curricula/static/curricula/js/components/answers/choices/image_with_text_choice.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
 export class ImageWithText extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = { checked: false }
+  }
+
+  cardClick (evt) {
+
+    this.setState({
+      checked: !this.state.checked
+    })
+    this.props.selectAnswer(this.props.choice.uuid, !this.state.checked)
+    evt.stopPropagation();
+    evt.preventDefault();
+  }
+
   render () {
     var style = {}
     if (this.props.hasAnswer) {
-      disabled = ' disabled'
+      var disabled = ' disabled'
       style['pointerEvents'] = 'none'
       if (this.props.wasResponse) {
         if (this.props.isAnswer) {
@@ -18,22 +34,30 @@ export class ImageWithText extends React.Component {
     var image = null
     if (this.props.choice.content.image){
       image = <img
-            className='inline-picture'
-            id={this.props.choice.uuid}
+            className='card-img-top img-responsive'
             style={style}
             src={this.props.choice.content.image}
-            onClick={this.props.checkAnswer}
           />
     }
-
+    //onClick={this.cardClick.bind(this)}
     return (
-      <span style={{display: 'inline-block'}}>
-        <div>{ image }</div>
-          <div><input id="checkBox" type="checkbox" />
-          {this.props.choice.content.text}
+      <div onClick={this.cardClick.bind(this)} className='card' style={{width: '20rem', float: 'left'}}  id={this.props.choice.uuid}>
+        { image }
+          <div className={'card-block'} style={{padding: '.5rem'}}>
+            <div className={'pure-checkbox'} style={{float: 'left'}}>
+              <input id={'checkbox'+this.props.choice.uuid} value={this.props.choice.content.text} type='checkbox' checked={this.state.checked}/>
+              <label htmlFor={'checkbox'+this.props.choice.uuid}>{this.props.choice.content.text}</label>
+              {/*{this.props.choice.content.text}*/}
+            </div>
+            <div onClick={this.cardClick.bind(this)}  style={{float: 'right'}}>
+              {this.props.index+1}
+            </div>
+            <div style={{clear: 'both'}}></div>
           </div>
-      </span>
+      </div>
     )
   }
-
+}
+ImageWithText.propTypes = {
+  selectAnswer: React.PropTypes.func.isRequired
 }
