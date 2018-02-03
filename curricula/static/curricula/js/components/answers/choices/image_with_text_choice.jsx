08b9c8dc2d159a4a2fa7a1ff3f452556c69ec9fb
LennyLip <vermus.jabber@gmail.com>
2018-02-03 10:52:23 +0300

    IMAGE_TEXT: set border around the image with both the width and height fixed

--- curricula/static/curricula/js/components/answers/choices/image_with_text_choice.jsx
+++ curricula/static/curricula/js/components/answers/choices/image_with_text_choice.jsx
@@ -18,31 +18,33 @@ export class ImageWithText extends React.Component {
   }
 
   render () {
-    var style = {}
+    var imgStyle = {}
     if (this.props.hasAnswer) {
       var disabled = ' disabled'
-      style['pointerEvents'] = 'none'
+      imgStyle['pointerEvents'] = 'none'
       if (this.props.wasResponse) {
         if (this.props.isAnswer) {
-          style['boxShadow'] = 'green 0px 0px 15px'
-          style['border'] = '2px solid rgb(79, 212, 24)'
+          imgStyle['boxShadow'] = 'green 0px 0px 15px'
+          imgStyle['border'] = '2px solid rgb(79, 212, 24)'
         } else if (this.props.wasResponse) {
-          style['boxShadow'] = 'rgb(255, 0, 0) 0px 0px 10px'
+          imgStyle['boxShadow'] = 'rgb(255, 0, 0) 0px 0px 10px'
         }
       }
     }
     var image = null
     if (this.props.choice.content.image){
       image = <img
-            className='card-img-top img-responsive'
-            style={style}
+            className='card-img-top img-fluid'
+            style={imgStyle}
             src={this.props.choice.content.image}
           />
     }
     //onClick={this.cardClick.bind(this)}
     return (
       <div onClick={this.cardClick.bind(this)} className='card' style={{width: '20rem', float: 'left'}}  id={this.props.choice.uuid}>
-        { image }
+         <div className='wrapper'>
+          { image }
+         </div>
           <div className={'card-block'} style={{padding: '.5rem'}}>
             <div className={'pure-checkbox'} style={{float: 'left'}}>
               <input id={'checkbox'+this.props.choice.uuid} value={this.props.choice.content.text} type='checkbox' checked={this.state.checked}/>
