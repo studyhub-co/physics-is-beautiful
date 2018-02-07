d87255b0966fc139f9d15d28fd5ed579ca75c175
LennyLip <vermus.jabber@gmail.com>
2018-02-07 07:27:06 +0300

    fix file name typo

--- curricula/models/__init__.py
+++ curricula/models/__init__.py
@@ -2,7 +2,7 @@ from .common import BaseModel, get_earliest_gap
 from .structure import Curriculum, Lesson, Game, Module, Unit
 from .question import Question
 from .answers import Answer, Image, MathematicalExpression, Text, Vector, UnitConversion, ImageWText
-from .user_respone import UserResponse
+from .user_response import UserResponse
 from .lesson_progress import LessonProgress
 
 __all__ = ["get_earliest_gap", "BaseModel",
