f9dde19f3e82869cafb5bcb6866967ff1a9fcd36
LennyLip <vermus.jabber@gmail.com>
2018-02-11 22:08:17 +0300

    ADMIN: auto check\uncheck answer for multi choices, backend validation

--- curricula/static/curricula/admin/js/automatic_save.js
+++ curricula/static/curricula/admin/js/automatic_save.js
@@ -1,5 +1,34 @@
 django.jQuery(function() {
-  django.jQuery("#id_answer_type" ).change(function() {
-    django.jQuery("input[name=_continue]").click();
-});
+
+  // autosave click
+  django.jQuery("#id_answer_type").change(function () {
+      django.jQuery("input[name=_continue]").click();
+  });
+
+  // auto multichoice
+  var ss = django.jQuery("#id_answer_type").val();
+  if (ss==100) { //only for multi choice
+
+    var selectedId = "";
+
+    django.jQuery("input[id^=id_answers-][name$=-is_correct]").change(function () {
+
+      if (django.jQuery(this).is(':checked')) {
+        selectedId = django.jQuery(this).attr('id');
+        // deselect others
+        django.jQuery("input[id^=id_answers-][name$=-is_correct]").each(function (e) {
+          if (selectedId != django.jQuery(this).attr('id')) {
+            django.jQuery(this).prop('checked', false);
+          }
+        })
+      } else { //select first
+          django.jQuery("input#id_answers-0-is_correct").prop('checked', true);
+      }
+    })
+
+    django.jQuery(".add-handler.djn-add-handler.djn-model-curricula-answer").click(function (e) {
+        django.jQuery("input#id_answers-__prefix__-is_correct").prop('checked', false);
+
+    });
+  }
 });
