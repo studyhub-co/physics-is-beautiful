05a5e0ca94d313cf1feb03b4dbd0caf0725ee6ad
LennyLip <vermus.jabber@gmail.com>
2018-02-25 10:14:33 +0300

    ADMIN: toggle answer details added

--- curricula/static/curricula/admin/js/question_admin.js
+++ curricula/static/curricula/admin/js/question_admin.js
@@ -31,4 +31,29 @@ django.jQuery(function() {
 
     });
   }
+
+  var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+  };
+
+  // hide question part if load from iframe
+  if(getUrlParameter('only-answers')){
+    django.jQuery("#header").hide();
+    django.jQuery("h1").hide();
+    django.jQuery(".object-tools").hide();
+    django.jQuery(".breadcrumbs").hide();
+    django.jQuery("fieldset.module").first().hide();
+  }
+
 });
