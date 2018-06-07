b11ba0459f0a26b24421701c329780fec66b9f0b
Alex Tereshkin <atereshkin@y-node.com>
2018-06-07 13:55:51 +0300

    Editor: Question editing mode layout improvements

--- editor/static/editor/js/components/mathematical_expression_answer.jsx
+++ editor/static/editor/js/components/mathematical_expression_answer.jsx
@@ -6,10 +6,8 @@ import {MQEditableLabel} from './mqlabel'
 export class MathematicalExpressionAnswer extends React.Component {
   render() {
     return (
-      <div>
-        <div className="col-md-1 module-accessible-block">
+        <div className="module-accessible-block mathematical-expression">
           <MQEditableLabel value={this.props.representation} onChange={this.props.onRepresentationChange}/>
-        </div>
-      </div>)    
+        </div>)    
   }
 }
