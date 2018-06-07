486ba91c4e848afbec78eddf1ca97beefc003728
Alex Tereshkin <atereshkin@y-node.com>
2018-06-07 13:02:53 +0300

    Editor: layout/apperance improvements in lesson editing mode

--- editor/static/editor/js/components/lesson.jsx
+++ editor/static/editor/js/components/lesson.jsx
@@ -21,7 +21,7 @@ export class Lesson extends React.Component {
       return <div>Loading...</div>
     }
     return (
-      <div>        
+      <div className="lesson">        
         <BackButton link={'/modules/' + this.props.module + '/'}/>
         <h1>
           <EditableThumbnail image={this.props.image} onChange={this.props.onImageChange}/>
@@ -31,12 +31,10 @@ export class Lesson extends React.Component {
         <label><input type="radio" value="0" checked={this.props.lesson_type==0} onChange={e=>this.props.onTypeChange(0)}/> Normal lesson</label>
         <label><input type="radio" value="1" checked={this.props.lesson_type==1} onChange={e=>this.props.onTypeChange(1)} /> Game</label>
         {this.props.lesson_type==1 &&
-          <label>Game type:
-              <select value={this.props.game_type} onChange={this.props.onGameTypeChange}>
-                  <option value="vector-game">Draw vector game</option>
-                  <option value="unit-conversion">Unit conversion game</option>
-                </select>
-            </label>
+          <select value={this.props.game_type} onChange={this.props.onGameTypeChange}>
+              <option value="vector-game">Draw vector game</option>
+                <option value="unit-conversion">Unit conversion game</option>
+            </select>
           }
       </div>)
   }
