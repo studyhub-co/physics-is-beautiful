cf1ab50e9bf721ff01130d139e287d43302d0c2b
Alex Tereshkin <atereshkin@y-node.com>
2018-06-07 13:58:10 +0300

    Editor: proper cursor over "add answer"

--- editor/static/editor/js/components/multiple_choice_answers.jsx
+++ editor/static/editor/js/components/multiple_choice_answers.jsx
@@ -12,7 +12,7 @@ export class MultipleChoiceAnswers extends React.Component {
     return <div>
       {answers}
       <div className="col-md-1 module-accessible-block">
-      <a onClick={this.props.onAddAnswerClick}><span className="glyphicon glyphicon-plus-sign"/> <br/>Add answer</a>
+      <a href="" onClick={e => {e.preventDefault();this.props.onAddAnswerClick()}}><span className="glyphicon glyphicon-plus-sign"/><br/>Add answer</a>
       </div>    
     </div>;
   }
