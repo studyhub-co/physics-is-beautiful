6e40439a7ba3d08c6f6d52ccef0c006683fb1895
Alex Tereshkin <atereshkin@y-node.com>
2018-06-10 18:32:38 +0300

    Editor: made math expression labels behave like regular ones (pointer cursor and editing on click)

--- editor/static/editor/js/components/mqlabel.jsx
+++ editor/static/editor/js/components/mqlabel.jsx
@@ -122,9 +122,10 @@ export class MQEditableLabel extends React.Component {
       }
       buttons = <span className="buttons">{btns}</span>
     }
-    return (<span className="mq-editable-label">
-            <span className={this.state.editing ? 'editing' : ''} ref={this.state.editing ? this.setEditorRef : this.setLabelRef} key={this.state.editing ? 'editing' : ''}>{this.props.value}</span>
-            <span onClick={this.handleEditClick} className="glyphicon glyphicon-pencil"/>
+    var isEmpty = !this.props.value || (this.props.value.replace(/\\/g, '').trim().length == 0)
+    return (<span className={'mq-editable-label' + (isEmpty ? ' empty' : '')}>
+            <span onClick={this.handleEditClick} className={this.state.editing ? 'editing' : ''} ref={this.state.editing ? this.setEditorRef : this.setLabelRef} key={this.state.editing ? 'editing' : ''}>{this.props.value}</span>
+            {!this.state.editing && <span onClick={this.handleEditClick} className="glyphicon glyphicon-pencil"/>}
             {buttons}
             </span>)
   }
