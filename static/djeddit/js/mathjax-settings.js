dd35daa35255df5286b7d5fbe48442fa759be751
Neil Chowdhury <neil.chowdhury@outlook.com>
2018-07-18 20:13:29 -0700

    add latex support and chemical equations

---
+++ static/djeddit/js/mathjax-settings.js
@@ -0,0 +1,14 @@
+MathJax.Ajax.config.path["mhchem"] =
+  "https://cdnjs.cloudflare.com/ajax/libs/mathjax-mhchem/3.3.0";
+
+
+MathJax.Hub.Config({
+    extensions: ["tex2jax.js", "[mhchem]/mhchem.js"],
+    jax: ["input/TeX", "output/HTML-CSS"],
+    tex2jax: {
+      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+      processEscapes: true
+    },
+    "HTML-CSS": { fonts: ["TeX"] }
+});
