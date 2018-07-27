a5e5f0cbb10e28222ffa52f7b7f898d740357ff5
Neil Chowdhury <neil.chowdhury@outlook.com>
2018-07-26 23:31:39 -0700

    fix mathjax

--- pib_auth/models.py
+++ pib_auth/models.py
@@ -92,7 +92,7 @@ class User(PermissionsMixin, AbstractBaseUser):
 
     @property
     def username(self):
-        return 'user{}'.format(self.id)
+        return '{}'.format(self.id)
 
     def get_short_name(self):
         return self.first_name or "User"
