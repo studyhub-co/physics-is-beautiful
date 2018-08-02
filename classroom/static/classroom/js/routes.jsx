b95138a5049d14cdf148968ccbfc1abceb16b396
LennyLip <vermus.jabber@gmail.com>
2018-08-02 13:02:26 +0300

    classroom: join/edit/delete classroom finished

--- classroom/static/classroom/js/routes.jsx
+++ classroom/static/classroom/js/routes.jsx
@@ -5,6 +5,7 @@ import { BASE_URL } from './utils/config'
 
 export default(
   <Switch>
+    <Route path={BASE_URL + 'join/:joinCode/'} component={IndexView} />  // must be upper
     <Route path={BASE_URL} component={IndexView} />
     {/* <Route path='*' component={NotFoundView} /> */}
   </Switch>
