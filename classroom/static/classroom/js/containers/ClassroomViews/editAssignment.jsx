cec96623e58a69c1de49a145ed5814601932af93
LennyLip <vermus.jabber@gmail.com>
2018-08-03 14:10:02 +0300

    classroom: Assignment tree + validate Assignment form

--- classroom/static/classroom/js/containers/ClassroomViews/editAssignment.jsx
+++ classroom/static/classroom/js/containers/ClassroomViews/editAssignment.jsx
@@ -11,7 +11,7 @@ import TimePicker from 'react-bootstrap-time-picker'
 
 import DropdownTreeSelect from 'react-dropdown-tree-select'
 
-import { Grid, Row, Col, Button } from 'react-bootstrap'
+import { Grid, Row, Col } from 'react-bootstrap'
 
 // import { Grid, Row, Col, InputGroup, FormControl, Modal, Button } from 'react-bootstrap'
 
@@ -24,11 +24,20 @@ class EditAssignmentView extends React.Component {
 
   constructor (props) {
     super(props)
-    // this.handleTitleChange = this.handleTitleChange.bind(this)
-    // this.handleCreateAssigment = this.handleCreateAssigment.bind(this)
+    this.handleStartOn = this.handleStartOn.bind(this)
+    this.handleDueOn = this.handleDueOn.bind(this)
+    this.handleStartTimeChange = this.handleStartTimeChange.bind(this)
+    this.handleDueTimeChange = this.handleDueTimeChange.bind(this)
+    this.onLessonTreeChange = this.onLessonTreeChange.bind(this)
 
     this.state = {
-      lessonsTreeData: []
+      lessonsTreeData: [],
+      selectedLessons: [],
+      startDate: null,
+      dueDate: null,
+      startTime: 3600,
+      dueTime: 3600,
+      assignmentIsValid: false
     }
   }
 
@@ -36,11 +45,67 @@ class EditAssignmentView extends React.Component {
     this.props.curriculaActions.curriculaFetchExpandedCurriculum(this.props.classroomTeacher.curriculum.uuid)
   }
 
+  componentWillReceiveProps (props) {
+    if (props.curriculumExpanded && props.curriculumExpanded.units.length > 0 && this.state.lessonsTreeData.length === 0) {
+      // populate with modulesTreeData
+      var newLessonsTreeData = []
+      newLessonsTreeData = this.addChildren(props.curriculumExpanded.units)
+      this.setState({lessonsTreeData: newLessonsTreeData})
+    }
+  }
+
   handleStartOn (value) {
-    console.log(value)
+    this.setState({startDate: value}, this.validateAssignment)
   }
   handleDueOn (value) {
-    console.log(value)
+    this.setState({dueDate: value}, this.validateAssignment)
+  }
+
+  handleStartTimeChange (value) {
+    this.setState({startTime: value}, this.validateAssignment)
+  }
+
+  handleDueTimeChange (value) {
+    this.setState({dueTime: value}, this.validateAssignment)
+  }
+
+  copyValidateTree (value) {
+    this.copySelectedNodesToTree(this.state.lessonsTreeData)
+    this.validateAssignment()
+  }
+
+  onLessonTreeChange (currentNode, selectedNodes) {
+    this.setState({selectedLessons: selectedNodes}, this.copyValidateTree)
+  }
+
+  copySelectedNodesToTree (treeNodes) {
+    for (var x = 0; x < treeNodes.length; x++) {
+      treeNodes[x].checked = false // unchek by default
+      for (var y = 0; y < this.state.selectedLessons.length; y++) {
+        if (this.state.selectedLessons[y].value === treeNodes[x].value) {
+          // copy all data from changed node
+          // Object.assign(treeNodes[x], this.state.selectedLessons[y])
+          console.log(treeNodes[x].checked);
+          treeNodes[x].checked = this.state.selectedLessons[y].checked
+        }
+      }
+      if (treeNodes[x].hasOwnProperty('children')) {
+        // walk by children
+        this.copySelectedNodesToTree(treeNodes[x].children)
+      }
+    }
+  }
+
+  validateAssignment () {
+    if (!this.state.startDate ||
+        !this.state.dueDate ||
+        !this.state.startTime ||
+        !this.state.dueTime ||
+        this.state.selectedLessons.length === 0) {
+      this.setState({assignmentIsValid: false})
+    } else {
+      this.setState({assignmentIsValid: true})
+    }
   }
 
   addChildren (children) {
@@ -51,7 +116,6 @@ class EditAssignmentView extends React.Component {
         if (children[i].hasOwnProperty('modules')) { childrenAttr = 'modules' }
         if (children[i].hasOwnProperty('lessons')) { childrenAttr = 'lessons' }
         var newchildren = this.addChildren(children[i][childrenAttr])
-        // console.log(newchildren);
         data.push({
           label: children[i].name,
           value: children[i].uuid,
@@ -62,25 +126,11 @@ class EditAssignmentView extends React.Component {
       } else {
         data.push({
           label: children[i].name,
-          value: children[i].name,
+          value: children[i].uuid,
           disabled: false })
       }
     }
     return data
-  }
-
-  componentWillReceiveProps (props) {
-    if (props.curriculumExpanded && props.curriculumExpanded.units.length > 0 && this.state.lessonsTreeData.length === 0) {
-      // populate with modulesTreeData
-      var newLessonsTreeData = []
-
-      newLessonsTreeData = this.addChildren(props.curriculumExpanded.units)
-      this.setState({lessonsTreeData: newLessonsTreeData})
-    }
-  }
-
-  validateAssignment () {
-
   }
 
   render () {
@@ -91,7 +141,7 @@ class EditAssignmentView extends React.Component {
             Assignment
           </Col>
           <Col sm={9} md={9}>
-            <DropdownTreeSelect data={this.state.lessonsTreeData} placeholderText={'Pick a goal skill'} />
+            <DropdownTreeSelect onChange={this.onLessonTreeChange} data={this.state.lessonsTreeData} placeholderText={'Pick a goal skill'} />
           </Col>
         </Row>
         <br />
@@ -100,10 +150,15 @@ class EditAssignmentView extends React.Component {
             Start on
           </Col>
           <Col sm={6} md={6}>
-            <DatePicker onChange={this.handleStartOn} />
+            <DatePicker onChange={this.handleStartOn} value={this.state.startDate} />
           </Col>
           <Col sm={3} md={3}>
-            <TimePicker start='10:00' end='21:00' step={30} />
+            <TimePicker
+              onChange={this.handleStartTimeChange}
+              start='10:00'
+              end='21:00'
+              step={60}
+              value={this.state.startTime} />
           </Col>
         </Row>
         <br />
@@ -112,16 +167,21 @@ class EditAssignmentView extends React.Component {
             Due on
           </Col>
           <Col sm={6} md={6}>
-            <DatePicker onChange={this.handleDueOn} />
+            <DatePicker onChange={this.handleDueOn} value={this.state.dueDate} />
           </Col>
           <Col sm={3} md={3}>
-            <TimePicker start='10:00' end='21:00' step={30} />
+            <TimePicker
+              onChange={this.handleDueTimeChange}
+              start='10:00'
+              end='21:00'
+              step={60}
+              value={this.state.dueTime} />
           </Col>
         </Row>
         <br />
         <Row>
           <Col sm={12} md={12} className={'text-center'}>
-            <Button className={'classroom-common-button'}>Shedule assignment</Button>
+            <button className={'classroom-common-button' + (this.state.assignmentIsValid ? '' : ' disabled-button')} disabled={!this.state.assignmentIsValid}>Shedule assignment</button>
           </Col>
         </Row>
       </Grid>)
