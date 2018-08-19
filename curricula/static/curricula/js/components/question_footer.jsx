1ecdf81ba2a87cca48c8aa858784d989229d9e6f
Neil Chowdhury <neil.chowdhury@outlook.com>
2018-08-18 23:03:47 -0700

    center question footer text

--- curricula/static/curricula/js/components/question_footer.jsx
+++ curricula/static/curricula/js/components/question_footer.jsx
@@ -20,10 +20,10 @@ export class Footer extends React.Component {
     var checkMarks = ''
     var correctMessage = ''
     if (this.props.correct === true) {
-      checkMarks = (<span id='correct' className='glyphicon glyphicon-ok-sign pull-right' />)
+      checkMarks = (<span id='correct' className='glyphicon glyphicon-ok-sign pull-right' style={{fontSize: '35px'}} />)
       correctMessage = 'Correct'
     } else if (this.props.correct === false) {
-      checkMarks = (<span id='incorrect' className='glyphicon glyphicon-remove-sign pull-right' />)
+      checkMarks = (<span id='incorrect' className='glyphicon glyphicon-remove-sign pull-right'style={{fontSize: '35px'}} />)
       if (Array.isArray(this.props.correct_answer)) {
         var message = 'Incorrect, the correct answers are:'
         if (this.props.correct_answer.length == 1) {
@@ -50,37 +50,37 @@ export class Footer extends React.Component {
     }
 
     return (
-        <div id='footer' >
-          <div className='row'>
-            <div className='col-md-6 text-center' style={{height: (checkMarks ? 'auto' : 0)}}>
-              <div id='checkMarks'>{checkMarks}</div>
-              <div id='correctMessage'>{correctMessage}</div>
-            </div>
-            <div className='col-md-6 text-center'>
-              <div id='checkContainer'>
-                <CheckContinueButton
-                    checkAction={this.props.checkAction}
-                    continueAction={this.props.continueAction}
-                    isCheck={typeof this.props.correct === 'undefined'}
-                    disabledCheck={this.props.disabledCheck} />
-              </div>
-            </div>
+      <div id='footer' >
+        <div className='row'>
+          <div className='col-md-6 text-center' style={{height: (checkMarks ? 'auto' : 0), display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <div id='checkMarks'>{checkMarks}</div>
+            <div id='correctMessage'>{correctMessage}</div>
           </div>
-          <div className='progress-bottom-container'>
-            <div className='progress'>
-              <div
-                  className='progress-bar progress-bar-info progress-bar-striped'
-                  id='progressbar'
-                  role='progressbar'
-                  aria-valuenow='0'
-                  aria-valuemin='0'
-                  aria-valuemax='100'
-                  style={{width: this.props.progress + '%'}}>
-                <span className='sr-only' />
-              </div>
+          <div className='col-md-6 text-center'>
+            <div id='checkContainer'>
+              <CheckContinueButton
+                checkAction={this.props.checkAction}
+                continueAction={this.props.continueAction}
+                isCheck={typeof this.props.correct === 'undefined'}
+                disabledCheck={this.props.disabledCheck} />
             </div>
           </div>
         </div>
+        <div className='progress-bottom-container'>
+          <div className='progress'>
+            <div
+              className='progress-bar progress-bar-info progress-bar-striped'
+              id='progressbar'
+              role='progressbar'
+              aria-valuenow='0'
+              aria-valuemin='0'
+              aria-valuemax='100'
+              style={{width: this.props.progress + '%'}}>
+              <span className='sr-only' />
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
