952ae3975e542166b73621d657c8a89258e7c8ed
Neil Chowdhury <neil.chowdhury@outlook.com>
2018-08-27 13:21:29 -0700

    add mobile_next page

--- pib_auth/urls.py
+++ pib_auth/urls.py
@@ -7,4 +7,5 @@ app_name = 'pib_auth'
 urlpatterns = [
     url(r'^login-next/$', views.login_next, name='login-next'),
     url(r'^blank/$', views.blank, name='blank'),
+    url(r'^mobile-next/$', views.mobile_next, name='mobile-next'),
 ]
