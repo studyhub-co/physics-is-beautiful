48ebc6273d043a555fe70ad8ebb3dae8b75ea229
Neil Chowdhury <neil.chowdhury@outlook.com>
2018-08-27 13:17:56 -0700

    move blank page to pib_auth

--- pib_auth/urls.py
+++ pib_auth/urls.py
@@ -6,5 +6,5 @@ app_name = 'pib_auth'
 
 urlpatterns = [
     url(r'^login-next/$', views.login_next, name='login-next'),
-    # url(r'^blank/$', views.blank, name='blank'),
+    url(r'^blank/$', views.blank, name='blank'),
 ]
