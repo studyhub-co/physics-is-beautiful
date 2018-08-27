2ce41524ba66621063b142272a0992e2f3b0945d
Neil Chowdhury <neil.chowdhury@outlook.com>
2018-08-27 13:17:56 -0700

    move blank page to pib_auth

--- pib_auth/views.py
+++ pib_auth/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
 
 
 @login_required
@@ -9,6 +10,10 @@ from django.views.decorators.csrf import csrf_exempt
 def login_next(request):
     
     if 'HTTP_REFERER' in request.META and 'pib_mobile' in request.META.get('HTTP_REFERER'):
-        return HttpResponseRedirect(reverse('blog:blank'))
+        return HttpResponseRedirect(reverse('pib_auth:blank'))
 
     return HttpResponseRedirect(reverse('curricula:curriculum'))
+
+
+def blank(request):
+    return render(request, 'pib_auth/blank.html')
