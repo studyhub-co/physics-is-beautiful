8f0123bfaa70acbaa5a364f25f950868d1e8e7de
Neil Chowdhury <neil.chowdhury@outlook.com>
2018-08-29 21:29:25 -0700

    votepost on topic

--- djeddit/static/djeddit/js/topic.js
+++ djeddit/static/djeddit/js/topic.js
@@ -5,7 +5,28 @@
 window.topicFuncs = {
   deleteTopic: function (topic_title) {
     if (confirm('This will permanently delete this topic and all related threads')) {
-        window.location = window.TOPIC_URLS['deleteTopic'];
+      window.location = window.TOPIC_URLS['deleteTopic']
     }
+  },
+  votePost: function (element, post, upvoted, downvoted, url) {
+    console.log(upvoted, downvoted)
+    var vote = 0
+    if (upvoted) { vote += 1 }
+    if (downvoted) { vote -= 1 }
+    // var url = window.THREAD_URLS['votePost']
+    var params = {post: post, vote: vote}
+    console.log(url, params)
+    $.post(url, params, function (data) {
+      console.log(data)
+      let parent = $(element).parent()
+      // update arrow icons
+      var $upvoteIcon = parent.find('.djeddit-score-upvote')
+      var $downvoteIcon = parent.find('.djeddit-score-downvote')
+      if (vote === 1) { $upvoteIcon.addClass('color-primary') } else { $upvoteIcon.removeClass('color-primary') }
+      if (vote === -1) { $downvoteIcon.addClass('color-primary') } else { $downvoteIcon.removeClass('color-primary') }
+      // update post's displayed score
+      parent.find('.djeddit-score-number').text(data.score)
+    //   $post.find('>.bs-callout-main>.minicol>.post-score').text(data.score)
+    })
   }
-};
+}
