f342b45a2cbbcd91321de0f8b6b4df452f897e61
LennyLip <vermus.jabber@gmail.com>
2018-09-06 09:40:55 +0300

    classroom: teacher classrooms students lists

--- classroom/static/classroom/js/components/TeacherClassroomCard.jsx
+++ classroom/static/classroom/js/components/TeacherClassroomCard.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { Image } from 'react-bootstrap'
+
 export class TeacherClassroomCard extends React.Component {
   render () {
     var className = 'classroom-card'
-
+    var studentsProfileUrlMask = this.props.baseUrl + '/' + this.props.classroom.uuid + '/teacher/students/'
     return (
       <div className={className}>
         <div onClick={() => { this.props.onTitleClick(this.props.baseUrl + '/' + this.props.classroom.uuid + '/teacher/') }}>
           {this.props.classroom.name}</div>
         <div>{this.props.classroom.curriculum.name}</div>
-        <div>Students photos list</div>
+        <div>{this.props.classroom.less_students
+          ? <div style={{textAlign: 'left', padding: '1.5rem'}}>
+            { this.props.classroom.less_students.map(function (student, i) {
+              return <span
+                key={i}
+                style={{paddingRight: '1rem', paddingBottom: '1rem', display: 'inline-block', cursor: 'pointer'}}
+                onClick={() => { this.props.onTitleClick(studentsProfileUrlMask + student.username) }}>
+                <Image
+                  src={student.gravatar_url}
+                  title={student.display_name}
+                  style={{width: '4rem'}}
+                  rounded
+                />
+              </span>
+            }, this)}
+          </div> : null }
+        </div>
         <div>{this.props.classroom.count_students} student{this.props.classroom.count_students === 1 ? null : 's'}</div>
       </div>
     )
