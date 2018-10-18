ed7fe59dacc3a0504d2b1f695582a7dd60ee674a
LennyLip <vermus.jabber@gmail.com>
2018-10-18 10:25:26 +0300

    curricula browse: active button navigation fix

--- piblib/templatetags/urls_tags.py
+++ piblib/templatetags/urls_tags.py
@@ -7,12 +7,14 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def current_url_starts_with(context, *args):
     """ Check if the browse is currently at this supplied url"""
-    # TODO problem: studio url reverse with editor url
-    # need to add namespace to 'editor' url()
     for url in args:
         current_url = context['request'].path
         supplied_url = reverse(url)
+
+        # fix for /curriculum/* (curricula application) and /curriculum/profile/* (editor application)
+        if url == 'main_curricula:curriculum' and current_url.startswith('/curriculum/profile/'):
+            return ''
+
         if current_url.startswith(supplied_url):
             return ' active'
-        else:
-            return ''
+    return ''
