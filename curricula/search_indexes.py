2ce9aa20ff0d5b69088d651f37e1b82690eff7a6
LennyLip <vermus.jabber@gmail.com>
2018-10-18 15:37:46 +0300

    curricula browse: Full Text Search classes

--- curricula/search_indexes.py
+++ curricula/search_indexes.py
@@ -1,6 +1,6 @@
-import datetime
 from haystack import indexes
-from .models.structure import Curriculum
+from .models.structure import Curriculum, Unit, Module, Lesson
+from .models.question import Question
 
 
 class CurriculumIndex(indexes.SearchIndex, indexes.Indexable):
@@ -14,3 +14,36 @@ class CurriculumIndex(indexes.SearchIndex, indexes.Indexable):
 
     def index_queryset(self, using=None):
         return self.get_model().objects.filter(setting_publically=True)
+
+
+class UnitIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True)
+    name = indexes.CharField(model_attr='name')
+
+    def get_model(self):
+        return Unit
+
+
+class ModuleIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True)
+    name = indexes.CharField(model_attr='name')
+
+    def get_model(self):
+        return Module
+
+
+class LessonIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True)
+    name = indexes.CharField(model_attr='name')
+
+    def get_model(self):
+        return Lesson
+
+
+class QuestionIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True)
+    question = indexes.CharField(model_attr='text')
+    hint = indexes.CharField(model_attr='hint')
+
+    def get_model(self):
+        return Question
