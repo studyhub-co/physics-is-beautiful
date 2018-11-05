6d5b39a1c4d1393bc6674c32294bf6750a1edefc
nchowder <neil.chowdhury@outlook.com>
2018-11-04 22:35:29 -0500

    add mobile app to blog

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^$', views.blog_homepage, name='bloghomepage'),
     url(r'^collegescorecard-analysis/$', views.college_scorecard, name='collegescorecard-analysis'),
     url(r'^shankar/$', views.shankar, name='shankar'),
+    url(r'^mobile-app/$', views.mobile_app, name='mobile_app'),
     url(r'^collegescorecard/$', views.college_scorecard_app, name='collegescorecard'),
     url(r'^collegemap/$', views.college_map, name='collegemap'),
     url(r'^wordpairs/$', views.word_pairs, name='wordpairs'),
