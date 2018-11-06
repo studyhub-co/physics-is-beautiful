d04d1e8621a62b3ebc79868ac95c52d225ed17e3
LennyLip <vermus.jabber@gmail.com>
2018-11-06 18:02:39 +0300

    browse app: cover_photo, description added

--- curricula/models/user_related.py
+++ curricula/models/user_related.py
@@ -6,13 +6,13 @@ from . import BaseModel, Curriculum
 class CurriculumUserDashboard(BaseModel):
     profile = models.ForeignKey(
         'profiles.Profile', related_name='dashboard_curricula', on_delete=models.CASCADE
-        # will remove row if user will be delete
+        # will remove CurriculumUserDashboard object if a user will be deleted
     )
     curriculum = models.ForeignKey(Curriculum, related_name='curricula_user_dashboard', on_delete=models.CASCADE)
-    # will remove row if curriculum will be delete
+    # will remove CurriculumUserDashboard object if curriculum will be deleted
 
-    # updated_on - will be update when user complete lesson
-    # remove fom dashboard will remove CurriculumDashboard object
+    # updated_on - will be updated when a user completed a lesson
+
     class Meta:
         unique_together = (("profile", "curriculum"),)
         db_table = 'curricula_userdashboard'
