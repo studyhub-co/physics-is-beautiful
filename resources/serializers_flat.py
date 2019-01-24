d6b85db977c251bff632aa779e93404e02fce362
LennyLip <vermus.jabber@gmail.com>
2019-01-24 21:48:45 +0300

    resources: resource edit: drag-n-drop chapters

--- resources/serializers_flat.py
+++ resources/serializers_flat.py
@@ -1,9 +1,28 @@
+from django.db.models import F
+
 from rest_framework import serializers
 
 from .models import Resource, TextBookSolutionPDF, ResourceMetaData, TextBookChapter, TextBookProblem, TextBookSolution
 
 
 class TextBookChapterSerializerFlat(serializers.ModelSerializer):
+
+    def update(self, instance, validated_data):
+        if 'position' in validated_data and instance.position != validated_data['position']:
+            if instance.position != -1:
+                TextBookChapter.objects.filter(position__gte=validated_data['position'],
+                                               resource=validated_data.get('resource', instance.resource))\
+                    .update(position=F('position')+1)
+            else:
+                try:
+                    last_position = TextBookChapter.objects.filter(
+                        resource=validated_data.get('resource', instance.resource)
+                    ).last().position + 1
+                except TextBookChapter.DoesNotExist:
+                    last_position = 0
+                validated_data['position'] = last_position
+
+        return super().update(instance, validated_data)
 
     class Meta:
         model = TextBookChapter
--- resources/static/resources/js/containers/ResourceView/Components/chapter.jsx
+++ resources/static/resources/js/containers/ResourceView/Components/chapter.jsx
@@ -2,50 +2,54 @@ import React from 'react'
 
 import PropTypes from 'prop-types'
 
+import { DragSource } from 'react-dnd'
 import { Glyphicon } from 'react-bootstrap'
 
 import { EditableLabel } from '../../../utils/editableLabel'
-
+import { DragItemTypes } from '../../../dnd'
 import Problem from './problem'
 
-export default class Chapter extends React.Component {
+let ChapterClass = class Chapter extends React.Component {
   render () {
     return (
-      <div>
-        <span className={'blue-title'}>
+      this.props.connectDragPreview(
+        <div style={{display: this.props.isDragging ? 'none' : 'block'}}>
+          <span className={'blue-title'}>
+            {this.props.resourceEditMode
+              ? <div>
+                {this.props.connectDragSource(<span className='drag-handle' />)}
+                <EditableLabel
+                  value={this.props.chapter.title}
+                  onChange={(value) => { this.props.onChangeChapterValue(value, this.props.chapter.id) }}
+                  editMode={this.props.chapterEditModeId === this.props.chapter.id}
+                /></div> : this.props.chapter.title
+            }
+          </span>
           {this.props.resourceEditMode
-            ? <EditableLabel
-              value={this.props.chapter.title}
-              onChange={(value) => { this.props.onChangeChapterValue(value, this.props.chapter.id) }}
-              editMode={this.props.chapterEditModeId === this.props.chapter.id}
-            /> : this.props.chapter.title
+            ? <div // Add problem button
+              style={{paddingLeft: '2rem', cursor: 'pointer'}}
+              onClick={() => this.props.addProblemClick(this.props.chapter)}
+              className={'blue-text'}>
+              <Glyphicon glyph='plus' /> Add problem
+            </div>
+            : null
           }
-        </span>
-        {this.props.resourceEditMode
-          ? <div // Add problem button
-            style={{paddingLeft: '2rem', cursor: 'pointer'}}
-            onClick={() => this.props.addProblemClick(this.props.chapter)}
-            className={'blue-text'}>
-            <Glyphicon glyph='plus' /> Add problem
-          </div>
-          : null
-        }
-        { this.props.chapter.problems ? this.props.chapter.problems.map(function (problem, i) { // ============ problems
-          return <Problem
-            resource={this.props.resource}
-            resourceEditMode={this.props.resourceEditMode}
-            onChangeProblemTitle={this.props.onChangeProblemTitle}
-            key={problem.uuid}
-            problem={problem}
-          />
-        }, this)
-          : null
-        }
-      </div>)
+          { this.props.chapter.problems ? this.props.chapter.problems.map(function (problem, i) { // ============ problems
+            return <Problem
+              resource={this.props.resource}
+              resourceEditMode={this.props.resourceEditMode}
+              onChangeProblemTitle={this.props.onChangeProblemTitle}
+              key={problem.uuid}
+              problem={problem}
+            />
+          }, this)
+            : null
+          }
+        </div>))
   }
 }
 
-Chapter.propTypes = {
+ChapterClass.propTypes = {
   chapter: PropTypes.object,
   resource: PropTypes.object,
   chapterEditModeId: PropTypes.number,
@@ -54,3 +58,20 @@ Chapter.propTypes = {
   onChangeChapterValue: PropTypes.func.isRequired,
   addProblemClick: PropTypes.func.isRequired
 }
+
+const dragSource = {
+  beginDrag (props) {
+    return props.chapter
+    // return {id: props.chapter.id}
+  }
+}
+
+function collect (connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  }
+}
+
+export default DragSource(DragItemTypes.CHAPTER, dragSource, collect)(ChapterClass)
