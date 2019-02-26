4ca5361a9e41d0904a04e1b7845972a209824a2a
LennyLip <vermus.jabber@gmail.com>
2019-02-26 13:01:19 +0300

    resources: react-slick trying.

--- resources/static/resources/js/routes.jsx
+++ resources/static/resources/js/routes.jsx
@@ -10,7 +10,6 @@ export default(
     {/*<Route exact path={BASE_URL + ':resource_uuid/problems/:problem_uuid/solutions/:uuid'} component={TextBookSolutionView} />*/}
     {/*<Route exact path={BASE_URL + ':resource_uuid/problems/:uuid'} component={TextBookProblemView} />*/}
     {/*<Route exact path={BASE_URL + ':uuid/'} component={ResourceView} />*/}
-    /resources/resource_title/problems/problem_title/solutions/solution_title/solution_uud
     <Route
       exact
       path={BASE_URL + ':resource_title([A-Za-z0-9_-]+)/problems/:problem_title([A-Za-z0-9_-]+)/solutions/:solution_title([A-Za-z0-9_-]+)/:uuid'}
