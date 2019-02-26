1bc5461d3b22fcb57473b3687801cd0ddd321b19
LennyLip <vermus.jabber@gmail.com>
2019-02-26 19:31:41 +0300

    resources: slider + pagination

--- resources/static/resources/js/containers/IndexView/carouselArrows.jsx
+++ resources/static/resources/js/containers/IndexView/carouselArrows.jsx
@@ -1,21 +1,22 @@
-export function SampleNextArrow (props) {
-  const { className, style, onClick } = props
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: 'block', background: 'red' }}
-      onClick={onClick}
-    />
-  )
-}
-
-export function SamplePrevArrow (props) {
-  const { className, style, onClick } = props
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: 'block', background: 'green' }}
-      onClick={onClick}
-    />
-  )
-}
+// we can customize arrows buttons
+// export function SampleNextArrow (props) {
+//   const { className, style, onClick } = props
+//   return (
+//     <div
+//       className={className}
+//       style={{ ...style, display: 'block', background: 'red' }}
+//       onClick={onClick}
+//     />
+//   )
+// }
+//
+// export function SamplePrevArrow (props) {
+//   const { className, style, onClick } = props
+//   return (
+//     <div
+//       className={className}
+//       style={{ ...style, display: 'block', background: 'green' }}
+//       onClick={onClick}
+//     />
+//   )
+// }
