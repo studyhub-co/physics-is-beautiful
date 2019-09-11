d67bb66b7531bbf59329a9094138b4be5444f7cb
LennyLip <vermus.jabber@gmail.com>
2019-09-11 12:02:00 +0300

    editor: answers choices replace with react bootstrap cards

--- editor/static/editor/js/components/answer_choice.jsx
+++ editor/static/editor/js/components/answer_choice.jsx
@@ -5,28 +5,36 @@ import { FaTimes } from 'react-icons/fa'
 import {EditableThumbnail} from './thumbnail'
 import {EditableLabel} from './label'
 
+import Card from 'react-bootstrap/Card'
+
 export class AnswerChoice extends React.Component {
-  render() {
+  render () {
     var thumb = <EditableThumbnail image={this.props.image} onChange={this.props.onImageChange}/>
-    var selectionControl = <input type={this.props.exclusive ? 'radio':'checkbox'}  onChange={this.props.onSelectChange} value={this.props.text} checked={this.props.is_correct}/>
-    var label = <EditableLabel value={this.props.text} defaultValue="New answer" onChange={this.props.onTextChange}/>
-    // var deleteIcon = <span className="glyphicon glyphicon-remove" onClick={this.props.onDeleteClick}/>
+    var selectionControl = <input type={this.props.exclusive ? 'radio' : 'checkbox'} onChange={this.props.onSelectChange} value={this.props.text} checked={this.props.is_correct}/>
+    var label = <EditableLabel value={this.props.text} defaultValue='New answer' onChange={this.props.onTextChange}/>
+    // TODO we have no deleteClick function
     var deleteIcon = <FaTimes onClick={this.onDeleteClick} />
 
-          
-    if (this.props.withThumbnail)
-    return (
-      <div className="answer-choice editor-col-md-1 module-accessible-block with-image">
-        <div className="thumbnail">{thumb}</div>
-        <div>
-          {selectionControl} {label} {deleteIcon}
-        </div>
-      </div>)
-    else
-      return (
-        <div className="answer-choice answer-button without-image">
-          {selectionControl} {label} {thumb} {deleteIcon}
-        </div>)
-      
+    // if (this.props.withThumbnail) {
+    //   return (
+    //     <div className='answer-choice editor-col-md-1 module-accessible-block with-image'>
+    //       <div className='thumbnail'>{thumb}</div>
+    //       <div>
+    //         {selectionControl} {label} {deleteIcon}
+    //       </div>
+    //     </div>)
+    // } else {
+    //   return (
+    //     <div className='answer-choice answer-button without-image'>
+    //       {selectionControl} {label} {thumb} {deleteIcon}
+    //     </div>)
+    // }
+
+    return (<Card style={{ height: '13rem' }}>
+      <Card.Body>
+        {thumb} {selectionControl} {label} {deleteIcon}
+      </Card.Body>
+    </Card>
+    )
   }
 }
--- editor/static/editor/js/components/multiple_choice_answers.jsx
+++ editor/static/editor/js/components/multiple_choice_answers.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 
 import {AnswerChoiceContainer} from '../containers/answer_choice'
 
+import CardColumns from 'react-bootstrap/CardColumns'
+
 export class MultipleChoiceAnswers extends React.Component {
   render () {
     var answers = []
+    // TODO add deleteAnswer function
     for (var i in this.props.answers) {
-      answers.push(<AnswerChoiceContainer key={this.props.answers[i]} uuid={this.props.answers[i]} exclusive={this.props.exclusive} withThumbnail={this.props.hasPictures}/>)
+      answers.push(<AnswerChoiceContainer
+        key={this.props.answers[i]}
+        uuid={this.props.answers[i]}
+        exclusive={this.props.exclusive}
+        withThumbnail={this.props.hasPictures}/>)
     }
     return <div>
-      {answers}
+      <CardColumns>{answers}</CardColumns>
       <div className='editor-col-md-1 module-accessible-block add-answer' onClick={e => { e.preventDefault(); this.props.onAddAnswerClick() }}>
       + Add answer
       </div>
