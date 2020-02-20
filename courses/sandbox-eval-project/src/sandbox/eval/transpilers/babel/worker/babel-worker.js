f3e133959945bcd33257cf9a9c616a45da4e0024
LennyLip <vermus.jabber@gmail.com>
2020-02-20 19:25:25 +0300

    eval project Babel fix

--- courses/sandbox-eval-project/src/sandbox/eval/transpilers/babel/worker/babel-worker.js
+++ courses/sandbox-eval-project/src/sandbox/eval/transpilers/babel/worker/babel-worker.js
@@ -404,10 +404,15 @@ async function compile(code, customConfig, path, isV7) {
 }
 
 try {
+  // self.importScripts(
+  //   process.env.NODE_ENV === 'development'
+  //     ? `${process.env.CODESANDBOX_HOST || ''}/static/js/babel.7.3.4.min.js`
+  //     : `${process.env.CODESANDBOX_HOST || ''}/static/js/babel.7.3.4.min.js`
+  // );
   self.importScripts(
     process.env.NODE_ENV === 'development'
-      ? `${process.env.CODESANDBOX_HOST || ''}/static/js/babel.7.3.4.min.js`
-      : `${process.env.CODESANDBOX_HOST || ''}/static/js/babel.7.3.4.min.js`
+      ? `${process.env.CODESANDBOX_HOST || ''}/static/courses/js/codesandbox-apps/babel.7.3.4.min.js`
+      : `${process.env.CODESANDBOX_HOST || ''}/proxy/static/courses/js/codesandbox-apps/babel.7.3.4.min.js`
   );
 } catch (e) {
   console.error(e);
--- courses/sandbox-eval-project/src/sandbox/eval/transpilers/babel/worker/index.js
+++ courses/sandbox-eval-project/src/sandbox/eval/transpilers/babel/worker/index.js
@@ -1,16 +1,17 @@
-import loadPolyfills from '@codesandbox/common/lib/load-dynamic-polyfills';
+import loadPolyfills from '@codesandbox/common/lib/load-dynamic-polyfills'
 
 // TODO fix this
 // require('app/config/polyfills');
+// `${process.env.CODESANDBOX_HOST}/static/browserfs5/browserfs.min.js`
 
 self.importScripts(
-  `${process.env.CODESANDBOX_HOST}/static/browserfs5/browserfs.min.js`
-);
+  `${process.env.CODESANDBOX_HOST}/proxy/static/courses/js/codesandbox-apps/browserfs/release/browserfs.min.js`
+)
 
-self.process = self.BrowserFS.BFSRequire('process');
-self.Buffer = self.BrowserFS.BFSRequire('buffer').Buffer;
+self.process = self.BrowserFS.BFSRequire('process')
+self.Buffer = self.BrowserFS.BFSRequire('buffer').Buffer
 
 loadPolyfills().then(() => {
   // eslint-disable-next-line global-require
-  require('./babel-worker');
-});
+  require('./babel-worker')
+})
