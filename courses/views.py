cd125c1846fe745d9ff43d831c87ae359087a9c5
LennyLip <vermus.jabber@gmail.com>
2020-03-14 01:14:15 +0300

    sandbox: screenshots fixes

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,13 @@
 # import time
 # from selenium import webdriver
 # from selenium.webdriver.chrome.options import Options
-#
+
+import io
+
 from django.http import HttpResponse
 from django.urls import reverse
 
+from PIL import Image, ImageDraw, ImageFont
 
 # def get_sandbox_image(request, pt_uuid):
 #     # TODO cache it
@@ -22,7 +25,7 @@ from django.urls import reverse
 #     return response
 
 import asyncio
-from pyppeteer import launch
+from pyppeteer import launch, errors
 
 
 async def get_screen(screenshot_url):
@@ -32,11 +35,25 @@ async def get_screen(screenshot_url):
         handleSIGHUP=False,
         headless=True,
         timeout=0,
+        args=['--no-sandbox', '--disable-setuid-sandbox'],  # disable Chrome sandbox
         # args=['--disable-dev-shm-usage', ] - docker setting
     )
     page = await browser.newPage()
     await page.goto(screenshot_url, {'waitUntil': 'networkidle2', 'timeout': 0})
-    await page.waitForSelector('#root')
+    try:
+        await page.waitForSelector('#root')
+    except errors.TimeoutError:
+        img = Image.new('RGB', (648, 480), color=(255, 255, 255, 255))
+
+        d = ImageDraw.Draw(img)
+        font = ImageFont.truetype("arial.ttf", 32)
+        d.text((100, 200), "The screenshot is not available", fill=(0, 0, 0, 255), font=font)
+        img_byte_arr = io.BytesIO()
+        img.save(img_byte_arr, format='PNG')
+        img = img_byte_arr.getvalue()
+
+        return img
+
     screen = await page.screenshot()
     await page.close()
     await browser.close()
