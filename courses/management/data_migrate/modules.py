441b762534e7c9219da293a506537542ae48f49d
LennyLip <vermus.jabber@gmail.com>
2021-02-18 11:04:55 +0300

    data migrate script: copy_module

---
+++ courses/management/data_migrate/modules.py
@@ -0,0 +1,37 @@
+from ...models.structure import Module
+from ...models.badges import ModuleAwards
+
+
+def copy_module(unit, module):
+    # copy data
+    new_module = Module()
+    for field in module._meta.get_fields():  # True, False?
+        if field.name in ('id', 'uuid', 'unit', 'lessons', 'tagged_items', 'tags', 'moduleawards'):
+            continue
+
+        new_field_value = getattr(module, field.name)
+        setattr(new_module, field.name, new_field_value)
+
+    new_module.unit = unit
+    new_module.author = unit.author  # old units has no author, so use author from unit
+    new_module.save()
+
+    # copy module awards
+    module_awards = []
+    for award in module.moduleawards_set.all():
+        new_award = ModuleAwards(user=award.user,
+                                 module=new_module,
+                                 module_finished_badge=award.module_finished_badge,
+                                 module_completed_award=award.module_completed_award
+                                 )
+
+        module_awards.append(new_award)
+
+    ModuleAwards.objects.bulk_create(module_awards)
+
+    # copy tags
+    tags = module.tags.names()
+    new_module.tags.set(*tags, clear=True)
+
+    # copy lessons
+
--- courses/management/data_migrate/units.py
+++ courses/management/data_migrate/units.py
@@ -1,11 +1,13 @@
 from ...models.structure import Unit
 
+from .modules import copy_module
+
 
 def copy_unit(course, unit):
     # 1. copy data
     new_unit = Unit()
     for field in unit._meta.get_fields():  # True, False?
-        if field.name in ('id', 'uuid', 'modules', 'tagged_items', 'tags'):
+        if field.name in ('id', 'uuid', 'curricula', 'modules', 'tagged_items', 'tags'):
             continue
 
         new_field_value = getattr(unit, field.name)
@@ -20,3 +22,5 @@ def copy_unit(course, unit):
     new_unit.tags.set(*tags, clear=True)
 
     # copy modules
+    for module in unit.modules.all():
+        copy_module(new_unit, module)
